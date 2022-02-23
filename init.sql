1737ac58653746c7e610abcf944a1d6d9759b3a7
Idan Horovitz <idanhh55@gmail.com>
2022-02-24 01:33:43 +0200

    Create initail to SQL table

---
+++ init.sql
@@ -0,0 +1,8 @@
+create table if not exists randoms(
+    entry_id int auto_increment, 
+    random_num int not null,
+    created_at timestamp default current_timestamp,
+    primary key(entry_id)
+);
+
+GRANT ALL PRIVILEGES ON *.* TO 'app'@'%';
